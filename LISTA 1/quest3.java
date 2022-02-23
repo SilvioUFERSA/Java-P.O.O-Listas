8cd61fa4dcacf1c11e89b548527ba44f925550d6
SilvioUFERSA <silviosmsantos@gmail.com>
2022-02-23 10:41:55 -0300

    alterações simples

--- LISTA 1/quest3.java
+++ LISTA 1/quest3.java
@@ -9,31 +9,32 @@ public class quest3 {
            
             Scanner entrada = new Scanner(System.in);
 
-            int graus;
-            double radio, sen, cos, tg, sec, cossec, cotg;
+
+            double graus, radio, sen, cos, tg, sec, cossec, cotg;
 
             System.out.println("Digite o valor em graus: ");
             
-            graus = entrada.nextInt();
+            graus = entrada.nextDouble();
         
-             radio = (graus * Math.PI) / 180;
+            radio = (graus * Math.PI) / 180;
 
-            cos = Math.cos(graus);
-            sen = Math.sin(graus);
-            tg = Math.tan(graus);
+            cos = Math.cos(radio);
+            sen = Math.sin(radio);
+            tg = Math.tan(radio);
 
-            sec = 1 / cos;
-            cossec = 1 / sen;
-            cotg = 1 / tg;
+            sec = 1 / cos;      // secante é o inverso do cosseno
+            cossec = 1 / sen;   // cossec é o inverso do seno
+            cotg = 1 / tg;      // cotg é o inverso da tangente
 
-             System.out.printf("- %d ° eh : %.3f radianos \n", graus, radio);
-             System.out.printf("- cosseno de %d eh: %.6f \n", graus, cos );
-             System.out.printf("- seno de %d eh: %.6f \n", graus, sen );
-             System.out.printf("- tangente de %d eh: %.6f \n", graus, tg );
+             System.out.printf("- %.2f ° eh : %.3f radianos \n", graus, radio);
+
+             System.out.printf("- cosseno de %.2f eh: %.6f \n", graus, cos );
+             System.out.printf("- seno de %.2f eh: %.6f \n", graus, sen );
+             System.out.printf("- tangente de %.2f eh: %.6f \n", graus, tg );
              
-             System.out.printf("- secante de %d eh: %.6f \n", graus, sec );
-             System.out.printf("- cossecante de %d eh: %.6f \n", graus, cossec );
-             System.out.printf("- cotangente de %d eh: %.6f \n", graus, cotg );
+             System.out.printf("- secante de %.2f eh: %.6f \n", graus, sec );
+             System.out.printf("- cossecante de %.2f eh: %.6f \n", graus, cossec );
+             System.out.printf("- cotangente de %.2f eh: %.6f \n", graus, cotg );
 
              entrada.close();
         }
